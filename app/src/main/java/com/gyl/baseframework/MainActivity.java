dd7ed922fdc3bdbaedad13366f7831fc036893c2
guiyun.li <guiyun.li@aihuishou.com>
2019-03-09 15:03:05 +0800

    项目提交

---
+++ app/src/main/java/com/gyl/baseframework/MainActivity.java
@@ -0,0 +1,15 @@
+addpackage com.gyl.baseframework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gyl.baseframework.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
